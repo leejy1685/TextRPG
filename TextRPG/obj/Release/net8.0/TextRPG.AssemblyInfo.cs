fef3c9a03ea2a8e720264658f2fbe6643519f0b1
leejy1685 <ucc1685@naver.com>
2025-04-18 10:10:36 +0900

    test : 코드 점검
    코드 점검

--- TextRPG/obj/Release/net8.0/TextRPG.AssemblyInfo.cs
+++ TextRPG/obj/Release/net8.0/TextRPG.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("TextRPG")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51607edce0fc9502dc022096f4e5515bd65fde1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TextRPG")]
